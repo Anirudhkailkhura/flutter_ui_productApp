eb310cd18556e3431bd78db6da04cd0c73fb85dc
anirudh <anirudhkailkhura@gmail.com>
2023-05-20 10:48:34 +0530

    frist commit

---
+++ lib/core/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+const Color green = Color.fromARGB(255, 70, 34, 154);
+const Color white = Color.fromARGB(255, 255, 255, 255);
+const Color black = Color(0xFF000000);
+const Color gray = Color.fromARGB(255, 255, 255, 255);
+const Color lightBlack = Color(0xFF6C6C6C);
---
+++ lib/data/model_data.dart
@@ -0,0 +1,55 @@
+
+
+import '../model/model.dart';
+
+List<Model> models = [
+  Model(
+    name: 'Stylish Lamp',
+    image: [
+      'assets/images/img1.jpg',
+      'assets/images/img2-fotor-bg-remover-20230517115521.png',
+      'assets/images/img3-fotor-bg-remover-20230517115516.png',
+      'assets/images/img4-fotor-bg-remover-20230517115511.png',
+    ],
+    price: '\$20.00',
+    description: 'We have amazing Quality Lamp Wide range',
+    category: 'Lamp',
+  ),
+  Model(
+    name: 'Stylish Sofa',
+    image: [
+      'assets/images/img2-fotor-bg-remover-20230517115521.png',
+      'assets/images/img2-fotor-bg-remover-20230517115521.png',
+      'assets/images/img2-fotor-bg-remover-20230517115521.png',
+      'assets/images/img2-fotor-bg-remover-20230517115521.png',
+    ],
+    price: '\$250.00',
+    description:
+        """Amazing stylish in multiple colors choice we have for Most selling item of this year. we have easy return product Options at our all stores in UAE.""",
+    category: 'Sofa',
+  ),
+  Model(
+    name: 'Wooden Chair',
+    image: [
+      'assets/images/img2-fotor-bg-remover-20230517115521.png',
+      'assets/images/img2-fotor-bg-remover-20230517115521.png',
+      'assets/images/img2-fotor-bg-remover-20230517115521.png',
+      'assets/images/img2-fotor-bg-remover-20230517115521.png',
+    ],
+    price: '\$35.00',
+    description: 'Amazing stylish in multiple Most selling item of this',
+    category: 'Chair',
+  ),
+  Model(
+    name: 'Modern Table',
+    image: [
+      'assets/images/img2-fotor-bg-remover-20230517115521.png',
+      'assets/images/img2-fotor-bg-remover-20230517115521.png',
+      'assets/images/img2-fotor-bg-remover-20230517115521.png',
+      'assets/images/Item_4 (2).jpg',
+    ],
+    price: '\$20.00',
+    description: 'New style of tables for your Homes and office.',
+    category: 'Table',
+  ),
+];
